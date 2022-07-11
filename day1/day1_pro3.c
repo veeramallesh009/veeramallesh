618e2f130c032c1388c0351b5cce606abc3179de
veeramallesh009 <108972387+veeramallesh009@users.noreply.github.com>
2022-07-11 22:33:51 +0530

    Create day1_pro3.c

---
+++ day1/day1_pro3.c
@@ -0,0 +1,12 @@
+//The entire area of a circular garden is to be covered with grass by a Gardener.
+//The radius of the garden is given by the user input. Calculate the area that has to be covered.
+#include <stdio.h>
+
+int main()
+{
+   int x;
+   printf("enter the value ");
+   scanf("%d",&x);
+   float y=2*3.14*x*x;
+   printf("%f",y);
+}
